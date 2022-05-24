41cc3d19484fe8b05f38ef7485503232495df907
Luka Camus <chloe.camus@epitech.eu>
2022-05-24 17:13:44 +0200

    [MODIFY] :sparkles: --> alaises can work indefinitely

--- src/analyse_command/mods/apply_mods.c
+++ src/analyse_command/mods/apply_mods.c
@@ -9,6 +9,14 @@
 #include "mysh.h"
 #include "my.h"
 
+static int cmp_arrays(char **src, char **cmp)
+{
+    for (int i = 0; src[i] && cmp[i]; i++)
+        if (my_strcmp(src[i], cmp[i]))
+            return FAILURE;
+    return SUCCESS;
+}
+
 char **apply_mods(char **og, shell_t *shell)
 {
     char **new = NULL;
@@ -18,6 +26,10 @@ char **apply_mods(char **og, shell_t *shell)
         return og;
     if ((new = analyse_aliases(og[0], shell)))
         dest = concate_arrays(new, og + 1);
+    new = NULL;
+    while (dest && (new = analyse_aliases(dest[0], shell)) &&
+        cmp_arrays(new, dest))
+        dest = concate_arrays(new, dest + 1);
     if (!dest)
         return og;
     return dest;
